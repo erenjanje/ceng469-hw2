f933988ea025a19dd0a573355012fa230e32b788
erenjanje <erencan.ceyhan@hotmail.com>
2023-05-10 08:13:11 +0300

    Map Rendered Texture Test

--- shaders/car_fragment.glsl
+++ shaders/car_fragment.glsl
@@ -4,7 +4,8 @@ flat in vec3 vertex_normal;
 out vec4 frag_color;
 
 uniform sampler2D environment;
+uniform mat4 model;
 
 void main() {
-    frag_color = texture(environment, vertex_normal.xy);
+    frag_color = texture(environment, (inverse(model) * vec4(vertex_position, 1.0f)).xy);
 }
